dfef3b8eb5d5aa36eaabaf5a6d64fcfafc310c32
Tarmo <liivamagi.tarmo@gmail.com>
2024-06-12 13:26:08 +0300

    new usertable style with tailwind

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -4,5 +4,9 @@ interface Props{
 }
 
 export const Header: React.FC<Props> = (props: Props) => {
-    return <h1>Todos!</h1>
+    return (
+        <div className="text-md px-6 py-3 block bg-primary text-background w-2/4 h-28 rounded-t-xl text-center shadow-xl">
+                Todo Or Not Todo
+        </div>
+)
 }
--- src/Components/UserTable.tsx
+++ src/Components/UserTable.tsx
@@ -12,6 +12,29 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
 
+// {/* <div className="flex justify-center">
+// {/* <div className="">
+//   <Header />
+// </div> */}
+// <div className="w-2/4 ">
+// <TableContainer component={Paper}>
+// <Table sx={{ minWidth: 650 }} aria-label="simple table">
+//   <TableHead>
+//     <TableRow>
+//       <TableCell>User</TableCell>
+//       <TableCell>Email</TableCell>
+//       <TableCell>Phone</TableCell>
+//     </TableRow>
+//   </TableHead>
+//   <TableBody>
+//     {userTableInfo}
+//   </TableBody>
+// </Table>
+// </TableContainer>
+// </div>
+// </div> */}
+
+
 interface Props {
 
 }
@@ -47,38 +70,40 @@ export const UserTable: React.FC<Props> = (props: Props) => {
   },[users])
 
   const userTableInfo = users.map((user) => (
-    <TableRow key={user.id}
-    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-      <TableCell ><Link to={`/todos/${user.id}`}>{user.name}</Link></TableCell>
-      <TableCell>{user.email}</TableCell>
-      <TableCell>{user.phone.replace(/[.'']/g, '-')}</TableCell>
-    </TableRow>
+    <tr key={user.id}>
+      <td className="text-md px-6 py-3"><Link to={`/todos/${user.id}`}>{user.name}</Link></td>
+      <td className="text-md px-6 py-3">{user.email}</td>
+      <td className="text-md px-6 py-3">{user.phone.replace(/[.'']/g, '-')}</td>
+    </tr>
   ));
 
   return (
     <>
-    <div className="flex justify-center">
-      {/* <div className="">
-        <Header />
-    </div> */}
-    <div className="w-2/4 ">
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>User</TableCell>
-            <TableCell>Email</TableCell>
-            <TableCell>Phone</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
+<main className="mx-auto">
+  <Header/>
+    <div className="relative w-2/4 flex flex-col shadow-2xl mb-6 rounded-b-xl">
+    <div className="relative flex flex-col self-center">
+      <table className="table-auto">
+        <thead>
+          <tr className="border border-solid border-l-0 border-r-0 border-t-0">
+            <th className="text-md px-6 py-3">User</th>
+            <th className="text-md px-6 py-3">Email</th>
+            <th className="text-md px-6 py-3">Phone</th>
+          </tr>
+        </thead>
+        <tbody>
           {userTableInfo}
-        </TableBody>
-      </Table>
-    </TableContainer>
+        </tbody>
+      </table>
+      </div>
     </div>
-    </div>
+    </main>
+    
     </>
   );
 };
+
+
+
+
 
